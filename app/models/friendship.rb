6b2e49cf8246e436b8a0665a636436e678ae5213
David Fox Powell <dtothefp@gmail.com>
2014-01-14 13:03:55 -0500

    cleaned assets to remove hard coded fb app id

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ActiveRecord::Base
-  validates :user_id, :friend_id, presence: true
+  # validates :user_id, :friend_id, presence: true
 
   belongs_to :friend, class_name: "User"
   belongs_to :user
