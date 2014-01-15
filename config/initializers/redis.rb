0c66877ee7b3900c76da67a6c98a29eeebdc2c94
David Fox Powell <dtothefp@gmail.com>
2014-01-15 15:55:17 -0500

    hard coded url in redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
-# if Rails.env.production?
-#   uri = URI.parse(ENV["REDISTOGO_URL"])
-#   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if Rails.env.production?
+  uri = URI.parse(redis://redistogo:b8bf1e5f6b7b9f0a577e53ac4c4e8243@grideye.redistogo.com:9664/)
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-#   Resque.redis = REDIS
-# end
+  Resque.redis = REDIS
+end
