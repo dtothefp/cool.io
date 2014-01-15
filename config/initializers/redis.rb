9d87a737a7d7a87b01dded5852a94bcd4e9f5e45
David Fox Powell <dtothefp@gmail.com>
2014-01-15 16:12:59 -0500

    edited redis initializer and resque.rake file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,4 @@
-if Rails.env.production?
-  uri = URI.parse(redis://redistogo:b8bf1e5f6b7b9f0a577e53ac4c4e8243@grideye.redistogo.com:9664/)
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:b8bf1e5f6b7b9f0a577e53ac4c4e8243@grideye.redistogo.com:9664/"
 
-  Resque.redis = REDIS
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
