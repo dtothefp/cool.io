a231e9a5071f6a55ff0d431200783892c0263f74
David Fox Powell <dtothefp@gmail.com>
2014-01-15 13:52:06 -0500

    check if heroku redis is connecting accidentally to localhost

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,6 @@
 if Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
+  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:3000/" )
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-  # Resque.redis = REDIS
+  Resque.redis = REDIS
 end
-
