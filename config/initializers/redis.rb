80c96bb5f9aacae26fcbc081786b7689a01518bc
David Fox Powell <dtothefp@gmail.com>
2014-01-15 13:45:33 -0500

    added Resque.redis = REDIS to redis initializery

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,8 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:3000/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+  Resque.redis = REDIS
+end
+
+# || "redis://localhost:3000/" 
