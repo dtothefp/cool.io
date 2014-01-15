6dc7fb6499f38aeb438be337929b74e422584ba6
David Fox Powell <dtothefp@gmail.com>
2014-01-15 11:27:04 -0500

    changed redis initializer file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,2 @@
-if Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  Resque.redis = REDIS
-end
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:3000/" )
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
