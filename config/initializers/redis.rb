3c959c4344400a850cc627a6c7b625f405b45708
David Fox Powell <dtothefp@gmail.com>
2014-01-15 14:04:11 -0500

    removed localhost option form redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:3000/" )
+  uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
   Resque.redis = REDIS
